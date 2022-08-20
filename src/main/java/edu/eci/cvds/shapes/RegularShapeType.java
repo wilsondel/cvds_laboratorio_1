897fc87b6b47af79050aae2372f736ef386dc8f6
clases clases <clases@labinfo.is.escuelaing.edu.co>
2022-08-20 10:02:12 -0500

    App skeleton initial state

---
+++ src/main/java/edu/eci/cvds/shapes/RegularShapeType.java
@@ -0,0 +1,5 @@
+package edu.eci.cvds.shapes;
+
+public enum RegularShapeType {
+    Triangle, Quadrilateral, Pentagon, Hexagon
+}
---
+++ src/main/java/edu/eci/cvds/shapes/Shape.java
@@ -0,0 +1,7 @@
+package edu.eci.cvds.shapes;
+
+
+
+public interface Shape {
+    public int getNumberOfEdges();
+}
